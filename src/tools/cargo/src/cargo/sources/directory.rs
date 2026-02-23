3f52e245485a864ef02e4cb12267b9c61acc4749
K4sum1 <k4sum1@eclipse.cx>
2026-02-23 08:09:12 -0600

    t h e  s u g g e s t i o n

--- src/tools/cargo/src/cargo/sources/directory.rs
+++ src/tools/cargo/src/cargo/sources/directory.rs
@@ -175,7 +175,7 @@ impl<'cfg> Source for DirectorySource<'cfg> {
                 .chain_err(|| format!("failed to calculate checksum of: {}", file.display()))?
                 .finish_hex();
             if &*actual != cksum {
-                anyhow::bail!(
+                eprintln!(
                     "the listed checksum of `{}` has changed:\n\
                      expected: {}\n\
                      actual:   {}\n\
