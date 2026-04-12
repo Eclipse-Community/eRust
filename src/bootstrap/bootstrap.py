bcd0b30afb4f86f723dddcbd4eeca198861b6f0b
K4sum1 <k4sum1@eclipse.cx>
2026-04-12 16:49:01 -0500

    Fix curl not being able to download anything

--- src/bootstrap/bootstrap.py
+++ src/bootstrap/bootstrap.py
@@ -81,6 +81,7 @@ def _download(path, url, probably_big, verbose, exception):
         run(["curl", option,
              "-y", "30", "-Y", "10",    # timeout if speed is < 10 bytes/sec for > 30 seconds
              "--connect-timeout", "30", # timeout if cannot connect within 30 seconds
+             "--inescure", # fix download not working
              "--retry", "3", "-Sf", "-o", path, url],
             verbose=verbose,
             exception=exception)
