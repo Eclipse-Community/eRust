3593af9e0103206c71c5c48dc46be57a9332b748
K4sum1 <k4sum1@eclipse.cx>
2026-04-27 02:34:49 -0500

    Feodor2 XP Rust patch (hope this all is it)
    I was doing a cherry-pick but GitHub Desktop said error no changes to commit and went like fuck you and cleared all my changes with no way to get them back. Except I had a src archive generated that I pulled the changes back from. I think this is it all, I hope. (This message is only for compiled 1.64.0, if I compile 1.65+ then this should not be an issue)

--- library/std/src/sys/windows/fs.rs
+++ library/std/src/sys/windows/fs.rs
@@ -13,7 +13,6 @@ use crate::sys::handle::Handle;
 use crate::sys::time::SystemTime;
 use crate::sys::{c, cvt};
 use crate::sys_common::{AsInner, FromInner, IntoInner};
-use crate::thread;
 
 use super::path::maybe_verbatim;
 use super::to_u16s;
@@ -308,18 +307,21 @@ impl File {
     pub fn truncate(&self, size: u64) -> io::Result<()> {
         let mut info = c::FILE_END_OF_FILE_INFO { EndOfFile: size as c::LARGE_INTEGER };
         let size = mem::size_of_val(&info);
-        cvt(unsafe {
-            c::SetFileInformationByHandle(
-                self.handle.as_raw_handle(),
-                c::FileEndOfFileInfo,
-                &mut info as *mut _ as *mut _,
-                size as c::DWORD,
-            )
-        })?;
-        Ok(())
+        unsafe {
+            let mut io: c::IO_STATUS_BLOCK = mem::zeroed();
+            if c::NtSetInformationFile(self.handle.as_raw_handle(),
+                                       &mut io as *mut _ as *mut _,
+                                       &mut info as *mut _ as *mut _,
+                                       size as c::DWORD,
+                                       20) == 0 {
+                Ok(())
+            }
+            else {
+                Err(crate::io::Error::last_os_error())
+            }
+        }
     }
 
-    #[cfg(not(target_vendor = "uwp"))]
     pub fn file_attr(&self) -> io::Result<FileAttr> {
         unsafe {
             let mut info: c::BY_HANDLE_FILE_INFORMATION = mem::zeroed();
@@ -347,56 +349,6 @@ impl File {
         }
     }
 
-    #[cfg(target_vendor = "uwp")]
-    pub fn file_attr(&self) -> io::Result<FileAttr> {
-        unsafe {
-            let mut info: c::FILE_BASIC_INFO = mem::zeroed();
-            let size = mem::size_of_val(&info);
-            cvt(c::GetFileInformationByHandleEx(
-                self.handle.as_raw_handle(),
-                c::FileBasicInfo,
-                &mut info as *mut _ as *mut libc::c_void,
-                size as c::DWORD,
-            ))?;
-            let mut attr = FileAttr {
-                attributes: info.FileAttributes,
-                creation_time: c::FILETIME {
-                    dwLowDateTime: info.CreationTime as c::DWORD,
-                    dwHighDateTime: (info.CreationTime >> 32) as c::DWORD,
-                },
-                last_access_time: c::FILETIME {
-                    dwLowDateTime: info.LastAccessTime as c::DWORD,
-                    dwHighDateTime: (info.LastAccessTime >> 32) as c::DWORD,
-                },
-                last_write_time: c::FILETIME {
-                    dwLowDateTime: info.LastWriteTime as c::DWORD,
-                    dwHighDateTime: (info.LastWriteTime >> 32) as c::DWORD,
-                },
-                file_size: 0,
-                reparse_tag: 0,
-                volume_serial_number: None,
-                number_of_links: None,
-                file_index: None,
-            };
-            let mut info: c::FILE_STANDARD_INFO = mem::zeroed();
-            let size = mem::size_of_val(&info);
-            cvt(c::GetFileInformationByHandleEx(
-                self.handle.as_raw_handle(),
-                c::FileStandardInfo,
-                &mut info as *mut _ as *mut libc::c_void,
-                size as c::DWORD,
-            ))?;
-            attr.file_size = info.AllocationSize as u64;
-            attr.number_of_links = Some(info.NumberOfLinks);
-            if attr.file_type().is_reparse_point() {
-                let mut b = [0; c::MAXIMUM_REPARSE_DATA_BUFFER_SIZE];
-                if let Ok((_, buf)) = self.reparse_point(&mut b) {
-                    attr.reparse_tag = buf.ReparseTag;
-                }
-            }
-            Ok(attr)
-        }
-    }
 
     pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
         self.handle.read(buf)
@@ -532,15 +484,19 @@ impl File {
             FileAttributes: perm.attrs,
         };
         let size = mem::size_of_val(&info);
-        cvt(unsafe {
-            c::SetFileInformationByHandle(
-                self.handle.as_raw_handle(),
-                c::FileBasicInfo,
-                &mut info as *mut _ as *mut _,
-                size as c::DWORD,
-            )
-        })?;
-        Ok(())
+        unsafe {
+            let mut io: c::IO_STATUS_BLOCK = mem::zeroed();
+            if c::NtSetInformationFile(self.handle.as_raw_handle(),
+                                       &mut io as *mut _ as *mut _,
+                                       &mut info as *mut _ as *mut _,
+                                       size as c::DWORD,
+                                       4) == 0 {
+                Ok(())
+            }
+            else {
+                Err(crate::io::Error::last_os_error())
+            }
+        }
     }
 
     pub fn set_times(&self, times: FileTimes) -> io::Result<()> {
@@ -555,226 +511,6 @@ impl File {
             c::SetFileTime(self.as_handle(), None, times.accessed.as_ref(), times.modified.as_ref())
         })?;
         Ok(())
-    }
-
-    /// Get only basic file information such as attributes and file times.
-    fn basic_info(&self) -> io::Result<c::FILE_BASIC_INFO> {
-        unsafe {
-            let mut info: c::FILE_BASIC_INFO = mem::zeroed();
-            let size = mem::size_of_val(&info);
-            cvt(c::GetFileInformationByHandleEx(
-                self.handle.as_raw_handle(),
-                c::FileBasicInfo,
-                &mut info as *mut _ as *mut libc::c_void,
-                size as c::DWORD,
-            ))?;
-            Ok(info)
-        }
-    }
-    /// Delete using POSIX semantics.
-    ///
-    /// Files will be deleted as soon as the handle is closed. This is supported
-    /// for Windows 10 1607 (aka RS1) and later. However some filesystem
-    /// drivers will not support it even then, e.g. FAT32.
-    ///
-    /// If the operation is not supported for this filesystem or OS version
-    /// then errors will be `ERROR_NOT_SUPPORTED` or `ERROR_INVALID_PARAMETER`.
-    fn posix_delete(&self) -> io::Result<()> {
-        let mut info = c::FILE_DISPOSITION_INFO_EX {
-            Flags: c::FILE_DISPOSITION_DELETE
-                | c::FILE_DISPOSITION_POSIX_SEMANTICS
-                | c::FILE_DISPOSITION_IGNORE_READONLY_ATTRIBUTE,
-        };
-        let size = mem::size_of_val(&info);
-        cvt(unsafe {
-            c::SetFileInformationByHandle(
-                self.handle.as_raw_handle(),
-                c::FileDispositionInfoEx,
-                &mut info as *mut _ as *mut _,
-                size as c::DWORD,
-            )
-        })?;
-        Ok(())
-    }
-
-    /// Delete a file using win32 semantics. The file won't actually be deleted
-    /// until all file handles are closed. However, marking a file for deletion
-    /// will prevent anyone from opening a new handle to the file.
-    fn win32_delete(&self) -> io::Result<()> {
-        let mut info = c::FILE_DISPOSITION_INFO { DeleteFile: c::TRUE as _ };
-        let size = mem::size_of_val(&info);
-        cvt(unsafe {
-            c::SetFileInformationByHandle(
-                self.handle.as_raw_handle(),
-                c::FileDispositionInfo,
-                &mut info as *mut _ as *mut _,
-                size as c::DWORD,
-            )
-        })?;
-        Ok(())
-    }
-
-    /// Fill the given buffer with as many directory entries as will fit.
-    /// This will remember its position and continue from the last call unless
-    /// `restart` is set to `true`.
-    ///
-    /// The returned bool indicates if there are more entries or not.
-    /// It is an error if `self` is not a directory.
-    ///
-    /// # Symlinks and other reparse points
-    ///
-    /// On Windows a file is either a directory or a non-directory.
-    /// A symlink directory is simply an empty directory with some "reparse" metadata attached.
-    /// So if you open a link (not its target) and iterate the directory,
-    /// you will always iterate an empty directory regardless of the target.
-    fn fill_dir_buff(&self, buffer: &mut DirBuff, restart: bool) -> io::Result<bool> {
-        let class =
-            if restart { c::FileIdBothDirectoryRestartInfo } else { c::FileIdBothDirectoryInfo };
-
-        unsafe {
-            let result = cvt(c::GetFileInformationByHandleEx(
-                self.handle.as_raw_handle(),
-                class,
-                buffer.as_mut_ptr().cast(),
-                buffer.capacity() as _,
-            ));
-            match result {
-                Ok(_) => Ok(true),
-                Err(e) if e.raw_os_error() == Some(c::ERROR_NO_MORE_FILES as _) => Ok(false),
-                Err(e) => Err(e),
-            }
-        }
-    }
-}
-
-/// A buffer for holding directory entries.
-struct DirBuff {
-    buffer: Vec<u8>,
-}
-impl DirBuff {
-    fn new() -> Self {
-        const BUFFER_SIZE: usize = 1024;
-        Self { buffer: vec![0_u8; BUFFER_SIZE] }
-    }
-    fn capacity(&self) -> usize {
-        self.buffer.len()
-    }
-    fn as_mut_ptr(&mut self) -> *mut u8 {
-        self.buffer.as_mut_ptr().cast()
-    }
-    /// Returns a `DirBuffIter`.
-    fn iter(&self) -> DirBuffIter<'_> {
-        DirBuffIter::new(self)
-    }
-}
-impl AsRef<[u8]> for DirBuff {
-    fn as_ref(&self) -> &[u8] {
-        &self.buffer
-    }
-}
-
-/// An iterator over entries stored in a `DirBuff`.
-///
-/// Currently only returns file names (UTF-16 encoded).
-struct DirBuffIter<'a> {
-    buffer: Option<&'a [u8]>,
-    cursor: usize,
-}
-impl<'a> DirBuffIter<'a> {
-    fn new(buffer: &'a DirBuff) -> Self {
-        Self { buffer: Some(buffer.as_ref()), cursor: 0 }
-    }
-}
-impl<'a> Iterator for DirBuffIter<'a> {
-    type Item = (&'a [u16], bool);
-    fn next(&mut self) -> Option<Self::Item> {
-        use crate::mem::size_of;
-        let buffer = &self.buffer?[self.cursor..];
-
-        // Get the name and next entry from the buffer.
-        // SAFETY: The buffer contains a `FILE_ID_BOTH_DIR_INFO` struct but the
-        // last field (the file name) is unsized. So an offset has to be
-        // used to get the file name slice.
-        let (name, is_directory, next_entry) = unsafe {
-            let info = buffer.as_ptr().cast::<c::FILE_ID_BOTH_DIR_INFO>();
-            let next_entry = (*info).NextEntryOffset as usize;
-            let name = crate::slice::from_raw_parts(
-                (*info).FileName.as_ptr().cast::<u16>(),
-                (*info).FileNameLength as usize / size_of::<u16>(),
-            );
-            let is_directory = ((*info).FileAttributes & c::FILE_ATTRIBUTE_DIRECTORY) != 0;
-
-            (name, is_directory, next_entry)
-        };
-
-        if next_entry == 0 {
-            self.buffer = None
-        } else {
-            self.cursor += next_entry
-        }
-
-        // Skip `.` and `..` pseudo entries.
-        const DOT: u16 = b'.' as u16;
-        match name {
-            [DOT] | [DOT, DOT] => self.next(),
-            _ => Some((name, is_directory)),
-        }
-    }
-}
-
-/// Open a link relative to the parent directory, ensure no symlinks are followed.
-fn open_link_no_reparse(parent: &File, name: &[u16], access: u32) -> io::Result<File> {
-    // This is implemented using the lower level `NtCreateFile` function as
-    // unfortunately opening a file relative to a parent is not supported by
-    // win32 functions. It is however a fundamental feature of the NT kernel.
-    //
-    // See https://docs.microsoft.com/en-us/windows/win32/api/winternl/nf-winternl-ntcreatefile
-    unsafe {
-        let mut handle = ptr::null_mut();
-        let mut io_status = c::IO_STATUS_BLOCK::default();
-        let name_str = c::UNICODE_STRING::from_ref(name);
-        use crate::sync::atomic::{AtomicU32, Ordering};
-        // The `OBJ_DONT_REPARSE` attribute ensures that we haven't been
-        // tricked into following a symlink. However, it may not be available in
-        // earlier versions of Windows.
-        static ATTRIBUTES: AtomicU32 = AtomicU32::new(c::OBJ_DONT_REPARSE);
-        let object = c::OBJECT_ATTRIBUTES {
-            ObjectName: &name_str,
-            RootDirectory: parent.as_raw_handle(),
-            Attributes: ATTRIBUTES.load(Ordering::Relaxed),
-            ..c::OBJECT_ATTRIBUTES::default()
-        };
-        let status = c::NtCreateFile(
-            &mut handle,
-            access,
-            &object,
-            &mut io_status,
-            crate::ptr::null_mut(),
-            0,
-            c::FILE_SHARE_DELETE | c::FILE_SHARE_READ | c::FILE_SHARE_WRITE,
-            c::FILE_OPEN,
-            // If `name` is a symlink then open the link rather than the target.
-            c::FILE_OPEN_REPARSE_POINT,
-            crate::ptr::null_mut(),
-            0,
-        );
-        // Convert an NTSTATUS to the more familiar Win32 error codes (aka "DosError")
-        if c::nt_success(status) {
-            Ok(File::from_raw_handle(handle))
-        } else if status == c::STATUS_DELETE_PENDING {
-            // We make a special exception for `STATUS_DELETE_PENDING` because
-            // otherwise this will be mapped to `ERROR_ACCESS_DENIED` which is
-            // very unhelpful.
-            Err(io::Error::from_raw_os_error(c::ERROR_DELETE_PENDING as _))
-        } else if status == c::STATUS_INVALID_PARAMETER
-            && ATTRIBUTES.load(Ordering::Relaxed) == c::OBJ_DONT_REPARSE
-        {
-            // Try without `OBJ_DONT_REPARSE`. See above.
-            ATTRIBUTES.store(0, Ordering::Relaxed);
-            open_link_no_reparse(parent, name, access)
-        } else {
-            Err(io::Error::from_raw_os_error(c::RtlNtStatusToDosError(status) as _))
-        }
     }
 }
 
@@ -1015,111 +751,30 @@ pub fn rmdir(p: &Path) -> io::Result<()> {
     Ok(())
 }
 
-/// Open a file or directory without following symlinks.
-fn open_link(path: &Path, access_mode: u32) -> io::Result<File> {
-    let mut opts = OpenOptions::new();
-    opts.access_mode(access_mode);
-    // `FILE_FLAG_BACKUP_SEMANTICS` allows opening directories.
-    // `FILE_FLAG_OPEN_REPARSE_POINT` opens a link instead of its target.
-    opts.custom_flags(c::FILE_FLAG_BACKUP_SEMANTICS | c::FILE_FLAG_OPEN_REPARSE_POINT);
-    File::open(path, &opts)
-}
-
 pub fn remove_dir_all(path: &Path) -> io::Result<()> {
-    let file = open_link(path, c::DELETE | c::FILE_LIST_DIRECTORY)?;
-
-    // Test if the file is not a directory or a symlink to a directory.
-    if (file.basic_info()?.FileAttributes & c::FILE_ATTRIBUTE_DIRECTORY) == 0 {
-        return Err(io::Error::from_raw_os_error(c::ERROR_DIRECTORY as _));
-    }
-
-    match remove_dir_all_iterative(&file, File::posix_delete) {
-        Err(e) => {
-            if let Some(code) = e.raw_os_error() {
-                match code as u32 {
-                    // If POSIX delete is not supported for this filesystem then fallback to win32 delete.
-                    c::ERROR_NOT_SUPPORTED
-                    | c::ERROR_INVALID_FUNCTION
-                    | c::ERROR_INVALID_PARAMETER => {
-                        remove_dir_all_iterative(&file, File::win32_delete)
-                    }
-                    _ => Err(e),
-                }
-            } else {
-                Err(e)
-            }
-        }
-        ok => ok,
+    let filetype = lstat(path)?.file_type();
+    if filetype.is_symlink() {
+        // On Windows symlinks to files and directories are removed differently.
+        // rmdir only deletes dir symlinks and junctions, not file symlinks.
+        rmdir(path)
+    } else {
+        remove_dir_all_recursive(path)
     }
 }
 
-fn remove_dir_all_iterative(f: &File, delete: fn(&File) -> io::Result<()>) -> io::Result<()> {
-    // When deleting files we may loop this many times when certain error conditions occur.
-    // This allows remove_dir_all to succeed when the error is temporary.
-    const MAX_RETRIES: u32 = 10;
-
-    let mut buffer = DirBuff::new();
-    let mut dirlist = vec![f.duplicate()?];
-
-    // FIXME: This is a hack so we can push to the dirlist vec after borrowing from it.
-    fn copy_handle(f: &File) -> mem::ManuallyDrop<File> {
-        unsafe { mem::ManuallyDrop::new(File::from_raw_handle(f.as_raw_handle())) }
-    }
-
-    let mut restart = true;
-    while let Some(dir) = dirlist.last() {
-        let dir = copy_handle(dir);
-
-        // Fill the buffer and iterate the entries.
-        let more_data = dir.fill_dir_buff(&mut buffer, restart)?;
-        restart = false;
-        for (name, is_directory) in buffer.iter() {
-            if is_directory {
-                let child_dir = open_link_no_reparse(
-                    &dir,
-                    name,
-                    c::SYNCHRONIZE | c::DELETE | c::FILE_LIST_DIRECTORY,
-                )?;
-                dirlist.push(child_dir);
-            } else {
-                for i in 1..=MAX_RETRIES {
-                    let result = open_link_no_reparse(&dir, name, c::SYNCHRONIZE | c::DELETE);
-                    match result {
-                        Ok(f) => delete(&f)?,
-                        // Already deleted, so skip.
-                        Err(e) if e.kind() == io::ErrorKind::NotFound => break,
-                        // Retry a few times if the file is locked or a delete is already in progress.
-                        Err(e)
-                            if i < MAX_RETRIES
-                                && (e.raw_os_error() == Some(c::ERROR_DELETE_PENDING as _)
-                                    || e.raw_os_error()
-                                        == Some(c::ERROR_SHARING_VIOLATION as _)) => {}
-                        // Otherwise return the error.
-                        Err(e) => return Err(e),
-                    }
-                    thread::yield_now();
-                }
-            }
-        }
-        // If there were no more files then delete the directory.
-        if !more_data {
-            if let Some(dir) = dirlist.pop() {
-                // Retry deleting a few times in case we need to wait for a file to be deleted.
-                for i in 1..=MAX_RETRIES {
-                    let result = delete(&dir);
-                    if let Err(e) = result {
-                        if i == MAX_RETRIES || e.kind() != io::ErrorKind::DirectoryNotEmpty {
-                            return Err(e);
-                        }
-                        thread::yield_now();
-                    } else {
-                        break;
-                    }
-                }
-            }
+fn remove_dir_all_recursive(path: &Path) -> io::Result<()> {
+    for child in readdir(path)? {
+        let child = child?;
+        let child_type = child.file_type()?;
+        if child_type.is_dir() {
+            remove_dir_all_recursive(&child.path())?;
+        } else if child_type.is_symlink_dir() {
+            rmdir(&child.path())?;
+        } else {
+            unlink(&child.path())?;
         }
     }
-    Ok(())
+    rmdir(path)
 }
 
 pub fn readlink(path: &Path) -> io::Result<PathBuf> {
@@ -1270,13 +925,12 @@ fn get_path(f: &File) -> io::Result<PathBuf> {
 }
 
 pub fn canonicalize(p: &Path) -> io::Result<PathBuf> {
-    let mut opts = OpenOptions::new();
-    // No read or write permissions are necessary
-    opts.access_mode(0);
-    // This flag is so we can open directories too
-    opts.custom_flags(c::FILE_FLAG_BACKUP_SEMANTICS);
-    let f = File::open(p, &opts)?;
-    get_path(&f)
+    let path = maybe_verbatim(p)?;
+    let mut file_part = ptr::null_mut();
+    super::fill_utf16_buf(
+        |buf, sz| unsafe { c::GetFullPathNameW(path.as_ptr(), sz, buf, &mut file_part) },
+        |buf| PathBuf::from(OsString::from_wide(buf)),
+    )
 }
 
 pub fn copy(from: &Path, to: &Path) -> io::Result<u64> {
--- library/std/src/sys/windows/locks/mutex.rs
+++ library/std/src/sys/windows/locks/mutex.rs
@@ -1,6 +1,6 @@
 //! System Mutexes
 //!
-//! The Windows implementation of mutexes is a little odd and it might not be
+//! The Windows implementation of mutexes is a little odd and it may not be
 //! immediately obvious what's going on. The primary oddness is that SRWLock is
 //! used instead of CriticalSection, and this is done because:
 //!
@@ -13,49 +13,135 @@
 //!
 //! 3. While CriticalSection is fair and SRWLock is not, the current Rust policy
 //!    is that there are no guarantees of fairness.
+//!
+//! The downside of this approach, however, is that SRWLock is not available on
+//! Windows XP, so we continue to have a fallback implementation where
+//! CriticalSection is used and we keep track of who's holding the mutex to
+//! detect recursive locks.
 
 use crate::cell::UnsafeCell;
+use crate::mem::{MaybeUninit};
+use crate::sync::atomic::{AtomicUsize, Ordering};
 use crate::sys::c;
 
 pub struct Mutex {
-    srwlock: UnsafeCell<c::SRWLOCK>,
+    lock: AtomicUsize,
+    held: UnsafeCell<bool>,
 }
-
 // Windows SRW Locks are movable (while not borrowed).
 pub type MovableMutex = Mutex;
 
 unsafe impl Send for Mutex {}
 unsafe impl Sync for Mutex {}
 
-#[inline]
-pub unsafe fn raw(m: &Mutex) -> c::PSRWLOCK {
-    m.srwlock.get()
-}
-
 impl Mutex {
     pub const fn new() -> Mutex {
-        Mutex { srwlock: UnsafeCell::new(c::SRWLOCK_INIT) }
+        Mutex {
+            // This works because SRWLOCK_INIT is 0 (wrapped in a struct), so we are also properly
+            // initializing an SRWLOCK here.
+            lock: AtomicUsize::new(0),
+            held: UnsafeCell::new(false),
+        }
     }
     #[inline]
     pub unsafe fn init(&mut self) {}
+    pub unsafe fn lock(&self) {
+                let re = self.remutex();
+                (*re).lock();
+                if !self.flag_locked() {
+                    (*re).unlock();
+                    panic!("cannot recursively lock a mutex");
+                }
+    }
+    pub unsafe fn try_lock(&self) -> bool {
+                let re = self.remutex();
+                if !(*re).try_lock() {
+                    false
+                } else if self.flag_locked() {
+                    true
+                } else {
+                    (*re).unlock();
+                    false
+                }
+    }
+    pub unsafe fn unlock(&self) {
+        *self.held.get() = false;
+            (*self.remutex()).unlock()
+    }
+    pub unsafe fn destroy(&self) {
+            match self.lock.load(Ordering::SeqCst) {
+                0 => {}
+                n => {
+                    Box::from_raw(n as *mut ReentrantMutex).destroy();
+                }
+            }
+    }
+
+    unsafe fn remutex(&self) -> *mut ReentrantMutex {
+        match self.lock.load(Ordering::SeqCst) {
+            0 => {}
+            n => return n as *mut _,
+        }
+        let re = box ReentrantMutex::uninitialized();
+        re.init();
+        let re = Box::into_raw(re);
+        match self.compare_and_swap(0, re as usize, Ordering::SeqCst) {
+            0 => re,
+            n => {
+                Box::from_raw(re).destroy();
+                n as *mut _
+            }
+        }
+    }
+
+    unsafe fn flag_locked(&self) -> bool {
+        if *self.held.get() {
+            false
+        } else {
+            *self.held.get() = true;
+            true
+        }
+    }
 
     #[inline]
+    fn compare_and_swap(&self, current: usize, new: usize, order: Ordering) -> usize {
+        match self.lock.compare_exchange(current, new, order, order) {
+            Ok(x) => x,
+            Err(x) => x,
+        }
+    }
+}
+
+pub struct ReentrantMutex {
+    inner: UnsafeCell<MaybeUninit<c::CRITICAL_SECTION>>,
+}
+
+unsafe impl Send for ReentrantMutex {}
+unsafe impl Sync for ReentrantMutex {}
+
+impl ReentrantMutex {
+    pub const fn uninitialized() -> ReentrantMutex {
+        ReentrantMutex { inner: UnsafeCell::new(MaybeUninit::uninit()) }
+    }
+
+    pub unsafe fn init(&self) {
+        c::InitializeCriticalSectionAndSpinCount((&mut *self.inner.get()).as_mut_ptr(), 2000);
+    }
+
     pub unsafe fn lock(&self) {
-        c::AcquireSRWLockExclusive(raw(self));
+        c::EnterCriticalSection((&mut *self.inner.get()).as_mut_ptr());
     }
 
     #[inline]
     pub unsafe fn try_lock(&self) -> bool {
-        c::TryAcquireSRWLockExclusive(raw(self)) != 0
+        c::TryEnterCriticalSection((&mut *self.inner.get()).as_mut_ptr()) != 0
     }
 
-    #[inline]
     pub unsafe fn unlock(&self) {
-        c::ReleaseSRWLockExclusive(raw(self));
+        c::LeaveCriticalSection((&mut *self.inner.get()).as_mut_ptr());
     }
 
-    #[inline]
     pub unsafe fn destroy(&self) {
-        // SRWLock does not need to be destroyed.
+        c::DeleteCriticalSection((&mut *self.inner.get()).as_mut_ptr());
     }
 }
